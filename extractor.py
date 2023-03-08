c4cca7452c4c963bb38a65ee0390baf8fe676f48
Daniel Strömberg <mail@danielst.se>
2023-03-08 23:10:56 +0100

    integraler

--- extractor.py
+++ extractor.py
@@ -17,7 +17,7 @@ def read_block(f):
             buf = buf + c;
     return buf
 
-sections = ["dfn", "thm", "nt", "ex"]
+sections = ["dfn", "thm", "nt"]
 indices = {}
 for s in sections:
     indices[s] = 0
